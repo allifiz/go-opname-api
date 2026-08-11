aeaa74ed8a797ad95e6417df47eae6a332a94b88
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-08-12 01:16:45 +0700

    add material usage revision endpoint

--- internal/handler/http/material_usage_handler.go
+++ internal/handler/http/material_usage_handler.go
@@ -13,6 +13,7 @@ func RegisterMaterialUsageRoutes(app *fiber.App, handler *MaterialUsageHandler) 
     api := app.Group("/api/v1")
     api.Post("/scheduled-menus/:id/material-usage", handler.Create)
     api.Get("/material-usages/:id", handler.Get)
+    api.Put("/material-usages/:id", handler.Update)
     api.Post("/material-usages/:id/submit", handler.Submit)
     api.Post("/material-usages/:id/decision", handler.Decide)
 }
@@ -27,6 +28,14 @@ func (h *MaterialUsageHandler) Create(c *fiber.Ctx) error {
 
 func (h *MaterialUsageHandler) Get(c *fiber.Ctx) error {
     data, err := h.service.Get(c.UserContext(), c.Params("id"))
+    if err != nil { return respondError(c, err) }
+    return c.JSON(fiber.Map{"data":data})
+}
+
+func (h *MaterialUsageHandler) Update(c *fiber.Ctx) error {
+    var input service.CreateMaterialUsageInput
+    if err := c.BodyParser(&input); err != nil { return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"invalid json body"}) }
+    data, err := h.service.Update(c.UserContext(), c.Params("id"), input)
     if err != nil { return respondError(c, err) }
     return c.JSON(fiber.Map{"data":data})
 }
