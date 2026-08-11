a40b4559a8cac6afafc03cab605655d899450cd0
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-08-12 01:48:01 +0700

    add jwt configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,6 +10,7 @@ import (
 type Config struct {
 	AppPort     string
 	DatabaseURL string
+	JWTSecret   string
 }
 
 func Load() (Config, error) {
@@ -18,10 +19,17 @@ func Load() (Config, error) {
 	cfg := Config{
 		AppPort:     getEnv("APP_PORT", "8080"),
 		DatabaseURL: os.Getenv("DATABASE_URL"),
+		JWTSecret:   os.Getenv("JWT_SECRET"),
 	}
 
 	if cfg.DatabaseURL == "" {
 		return Config{}, fmt.Errorf("DATABASE_URL is required")
+	}
+	if cfg.JWTSecret == "" {
+		return Config{}, fmt.Errorf("JWT_SECRET is required")
+	}
+	if len(cfg.JWTSecret) < 32 {
+		return Config{}, fmt.Errorf("JWT_SECRET must be at least 32 characters")
 	}
 
 	return cfg, nil
@@ -31,6 +39,5 @@ func getEnv(key, fallback string) string {
 	if value := os.Getenv(key); value != "" {
 		return value
 	}
-
 	return fallback
 }
