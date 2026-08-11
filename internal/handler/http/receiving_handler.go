6c789a83ab6032a05f5003685b91af5ce4b226a1
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-08-12 01:50:52 +0700

    enforce receiving rbac and jwt actor

--- internal/handler/http/receiving_handler.go
+++ internal/handler/http/receiving_handler.go
@@ -5,46 +5,23 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type ReceivingHandler struct {
-	service *service.ReceivingService
-}
-
-func NewReceivingHandler(service *service.ReceivingService) *ReceivingHandler {
-	return &ReceivingHandler{service: service}
-}
+type ReceivingHandler struct { service *service.ReceivingService }
+func NewReceivingHandler(service *service.ReceivingService) *ReceivingHandler { return &ReceivingHandler{service: service} }
 
 func RegisterReceivingRoutes(app *fiber.App, handler *ReceivingHandler) {
 	api := app.Group("/api/v1")
-
-	api.Post("/purchase-orders/:id/receipts", handler.CreateReceipt)
+	api.Post("/purchase-orders/:id/receipts", RequireRoles("PENGAWAS_BAHAN_BAKU"), handler.CreateReceipt)
 	api.Get("/purchase-orders/:id/receipts", handler.ListByPurchaseOrder)
 	api.Get("/receipts/:id", handler.GetReceipt)
 }
 
 func (h *ReceivingHandler) CreateReceipt(c *fiber.Ctx) error {
 	var input service.CreateReceiptInput
-	if err := c.BodyParser(&input); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid json body"})
-	}
+	if err := c.BodyParser(&input); err != nil { return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"invalid json body"}) }
+	input.ReceivedBy = ActorID(c)
 	data, err := h.service.CreateReceipt(c.UserContext(), c.Params("id"), input)
-	if err != nil {
-		return respondError(c, err)
-	}
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"data": data})
+	if err != nil { return respondError(c, err) }
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"data":data})
 }
-
-func (h *ReceivingHandler) ListByPurchaseOrder(c *fiber.Ctx) error {
-	data, err := h.service.ListByPurchaseOrder(c.UserContext(), c.Params("id"))
-	if err != nil {
-		return respondError(c, err)
-	}
-	return c.JSON(fiber.Map{"data": data})
-}
-
-func (h *ReceivingHandler) GetReceipt(c *fiber.Ctx) error {
-	data, err := h.service.GetReceipt(c.UserContext(), c.Params("id"))
-	if err != nil {
-		return respondError(c, err)
-	}
-	return c.JSON(fiber.Map{"data": data})
-}
+func (h *ReceivingHandler) ListByPurchaseOrder(c *fiber.Ctx) error { data, err := h.service.ListByPurchaseOrder(c.UserContext(), c.Params("id")); if err != nil { return respondError(c, err) }; return c.JSON(fiber.Map{"data":data}) }
+func (h *ReceivingHandler) GetReceipt(c *fiber.Ctx) error { data, err := h.service.GetReceipt(c.UserContext(), c.Params("id")); if err != nil { return respondError(c, err) }; return c.JSON(fiber.Map{"data":data}) }
